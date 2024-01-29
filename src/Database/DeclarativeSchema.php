e3c93b78c20a6102a34510ea8bc20f1bb614a303
MichelJonkman <mj.99@live.nl>
2024-01-29 20:30:32 +0100

    Removed dangerous "existing" option

--- src/Database/DeclarativeSchema.php
+++ src/Database/DeclarativeSchema.php
@@ -7,22 +7,5 @@ use Doctrine\DBAL\Schema\AbstractSchemaManager;
 
 abstract class DeclarativeSchema
 {
-    protected ?AbstractSchemaManager $schemaManager = null;
-
-    public function init(AbstractSchemaManager $schemaManager): void
-    {
-        $this->schemaManager = $schemaManager;
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function existing(string $name): Table
-    {
-        $table = $this->schemaManager->introspectTable($name);
-
-        return new Table($table->getName(), $table->getColumns(), $table->getIndexes(), $table->getUniqueConstraints(), $table->getForeignKeys(), $table->getOptions());
-    }
-
     abstract function declare(): Table;
 }
